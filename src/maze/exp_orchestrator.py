85596eadc9af8b709c6f24cc3c7c6236aed61d7a
Fei Phoon <fei.phoon@gmail.com>
2021-04-10 19:07:01 +0100

    Consolidate functions

--- src/maze/exp_orchestrator.py
+++ src/maze/exp_orchestrator.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-from exp_runner import run_single_exp
 
-# from maze import Maze
+def run_single_exp(env, policy):
+    obs = env.reset()
+    _done = False
+
+    total_reward = 0
+
+    while not _done:
+        action = policy(obs)
+        obs, reward, _done = env.step(action)
+
+        total_reward += reward
+
+    print(env.turns_elapsed)
+    return total_reward
 
 
 def run_experiments(env, policy, num_exp):
